9535b771383c2d3ba1b07b27de8f8f6356121f77
madipfaff <mpfaff@587234161.guest.umich.net>
2015-09-12 02:51:16 -0400

    Initial Commit

---
+++ 2werk/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  2werk
+//
+//  Created by Madison Pfaff on 9/12/15.
+//
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
